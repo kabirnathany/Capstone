c809766632f6d8f1c5d2b93600ce21f3d783010b
Kabir Nathany <kabirnathany@Kabirs-Mac-mini.local>
2025-07-14 09:59:14 -0400

    Set up semantic structure, add components, and meta/OGP tags

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <img src="logo192.png" alt="Little Lemon Logo" style={{ height: '60px' }} />
+    </header>
+  );
+}
+
+export default Header; 
